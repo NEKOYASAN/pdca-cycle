263ee1f2fb58b3608fc9edc4b5ca9bade6f9a100
NEKOYASAN <hello@nekoyasan.me>
2022-04-26 14:49:36 +0900

    add ogp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
 import { Box, Flex, keyframes, Text } from '@chakra-ui/react';
 import { useEffect, useRef, useState } from 'react';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   const windowRef = useRef(null);
-  const [width, setWidth] = useState(100);
-  const [height, setHeight] = useState(100);
+  const [width, setWidth] = useState<number | null>(null);
+  const [height, setHeight] = useState<number | null>(null);
   useEffect(() => {
     const windowRefObserver = new ResizeObserver((entries) => {
       setWidth(entries[0].contentRect.width);
@@ -33,37 +34,53 @@ const Home: NextPage = () => {
       justifyContent={'center'}
       alignItems={'center'}
     >
-      <Flex
-        width={width < height ? width / 1.42 - 25 + 'px' : height / 1.42 - 25 + 'px'}
-        height={width < height ? width / 1.42 - 25 + 'px' : height / 1.42 - 25 + 'px'}
-        flexDirection={'column'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        animation={`1s linear infinite ${rotate}`}
-      >
-        <Text
-          fontWeight={900}
-          fontSize={
-            (width / 1.42 - 25) / 2 < (height / 1.42 - 25) / 2
-              ? (width / 1.42 - 25) / 2 + 'px'
-              : (height / 1.42 - 25) / 2 + 'px'
-          }
-          lineHeight={0.8}
+      <Head>
+        <title>PDCA Cycle</title>
+        <meta property="og:url" content="https://www.pdca-cycle.jp/" />
+
+        <meta property="og:type" content="website" />
+
+        <meta property="og:title" content="PDCA Cycle" />
+
+        <meta property="og:description" content="PDCA 回さない？" />
+
+        <meta property="og:site_name" content="PDCA Cycle" />
+
+        <meta property="og:image" content="https://www.pdca-cycle.jp/ogp.png" />
+      </Head>
+      {width && height ? (
+        <Flex
+          width={width < height ? width / 1.42 - 25 + 'px' : height / 1.42 - 25 + 'px'}
+          height={width < height ? width / 1.42 - 25 + 'px' : height / 1.42 - 25 + 'px'}
+          flexDirection={'column'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          animation={`1s linear infinite ${rotate}`}
         >
-          PD
-        </Text>
-        <Text
-          fontWeight={900}
-          fontSize={
-            (width - 25) / 2 < (height / 1.42 - 25) / 2
-              ? (width / 1.42 - 25) / 2 + 'px'
-              : (height / 1.42 - 25) / 2 + 'px'
-          }
-          lineHeight={0.8}
-        >
-          CA
-        </Text>
-      </Flex>
+          <Text
+            fontWeight={900}
+            fontSize={
+              (width / 1.42 - 25) / 2 < (height / 1.42 - 25) / 2
+                ? (width / 1.42 - 25) / 2 + 'px'
+                : (height / 1.42 - 25) / 2 + 'px'
+            }
+            lineHeight={0.8}
+          >
+            PD
+          </Text>
+          <Text
+            fontWeight={900}
+            fontSize={
+              (width - 25) / 2 < (height / 1.42 - 25) / 2
+                ? (width / 1.42 - 25) / 2 + 'px'
+                : (height / 1.42 - 25) / 2 + 'px'
+            }
+            lineHeight={0.8}
+          >
+            CA
+          </Text>
+        </Flex>
+      ) : null}
     </Flex>
   );
 };
