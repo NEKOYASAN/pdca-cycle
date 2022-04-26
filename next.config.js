c06eb3ebb9309a503d2f314684aa85788b320ccc
NEKOYASAN <hello@nekoyasan.me>
2022-04-26 14:36:42 +0900

    pdca-cycle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: false,
+  poweredByHeader: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
